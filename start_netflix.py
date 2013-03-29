bde8e63fdc0645343cf4ad54e6352fd3bb3463cf
Ben Ng <gphonkn@gmial.com>
2013-03-28 22:14:50 -0400

    testing

---
+++ start_netflix.py
@@ -0,0 +1,10 @@
+import netflix
+
+api = netflix.NetflixAPI(api_key='y56g4wvr49qkecdkw6uev66p',
+               api_secret='FKA3C3Zcjv')
+
+d = api.get("catalog/titles/autocomplete", {"term" : "spider"})
+
+for title_info in d['autocomplete']['autocomplete_item']:
+	print title_info['title']['short']
+
