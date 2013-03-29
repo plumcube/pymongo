523bf35b15ce9e119c1723febd905304df6b17bc
Ben <ben@ben-samsung.(none)>
2013-03-29 13:48:01 -0400

    Added debug message

--- start_netflix.py
+++ start_netflix.py
@@ -1,5 +1,7 @@
 import netflix
 
+print 'start'
+
 api = netflix.NetflixAPI(api_key='y56g4wvr49qkecdkw6uev66p',
                api_secret='FKA3C3Zcjv')
 
@@ -8,3 +10,4 @@ d = api.get("catalog/titles/autocomplete", {"term" : "spider"})
 for title_info in d['autocomplete']['autocomplete_item']:
 	print title_info['title']['short']
 
+print 'finish'
